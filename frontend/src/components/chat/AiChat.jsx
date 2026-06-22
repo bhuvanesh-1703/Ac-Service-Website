cdcc168dc88746f8686646570178a1a25e0a31e2
bhuvanesh-1703 <bhuvaneshmahi2003@gmail.com>
2026-06-22 17:47:26 +0530

    feat: implement AI chat interface and responsive navigation bar component

--- frontend/src/components/chat/AiChat.jsx
+++ frontend/src/components/chat/AiChat.jsx
@@ -4,9 +4,45 @@ import { useState } from 'react'
 const AiChat = () => {
 
     const [isOpen,setIsOpen]=useState(false);
-    const [message,setMesssage]=usestate('')
+    const [message,setMesssage]=useState('')
+    const [chatHistory,setChatHistory]=useState([])
+    const [loading,setLoading]=useState(false)
+
+
+    const handleSendMessage=async(e)=>{
+        e.preventDefault()
+    if(message===""){
+        return;
+    }
+    const userMessage = {role:user,parts:[{text:message}]}
+    const newHistory = [...chatHistory,userMessage];
+    setChatHistory(newHistory);
+    setMesssage('')
+    setLoading(true)
+        try {
+            const response=await axios.post('http://localhost:5100/api/chat',{
+                method:'POST',
+                header:{
+                    'Content-Type':'application/json'
+                },
+                body:JSON.stringify({message:message,istory:chatHistory})
+            })
+            const datas= await response.json()
+
+            const aiMessage={role:'model',parts:[{text:data.data}]}
+            setChatHistory([...newHistory,aiMessage])
+            
+        } catch (error) {
+            console.error("error",error)
+        } finally{
+            setLoading(false)
+        }
+    }
   return (
    <>
+   <button onClick={()=>setIsOpen(!isopen)}>
+    {isOpen?"Close Chat":"Open Chat"}
+   </button>
    
    </>
   )
